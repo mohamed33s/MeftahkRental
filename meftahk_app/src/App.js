5901c13f9c9ced6cf594926c41e5668919c627e1
mohamed sulaiman <mohamed@outlook.sa>
2024-05-11 17:08:26 +0300

    Edit V2.1

--- meftahk_app/src/App.js
+++ meftahk_app/src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import Cars from "./Components/Cars";
+import { BrwoserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-
-      <div className="titleTxt">
-        <p>اختر سيارتك</p>
-      </div>
+      <Router>
+        <NavBar />
+        <div className="titleTxt">
+          <p>اختر سيارتك</p>
+        </div>
+        <Routes>
+          <Route path = "/" element={<Cars/>} />
+          <Route path = "/" element = {<Cars />} />
+          <Route path = "/" element = {<Cars />} />
+        </Routes>
+      </Router>
 
       <Cars />
     </div>
